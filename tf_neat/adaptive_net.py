4e5d056b177a650d9963d879eabeb586e7031075
Cristian Bodnar <crisbodnar96@gmail.com>
2018-12-24 14:01:03 +0200

    Modify adaptive net

--- tf_neat/adaptive_net.py
+++ tf_neat/adaptive_net.py
@@ -12,14 +12,14 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
-import torch
+import tensorflow as tf
 from .activations import tanh_activation
 from .cppn import create_cppn, clamp_weights_, get_coord_inputs
+from .helpers import expand
 
 
 class AdaptiveNet:
     def __init__(self,
-
                  w_ih_node,
                  b_h_node,
                  w_hh_node,
@@ -38,80 +38,67 @@ class AdaptiveNet:
                  batch_size=1,
                  device='cuda:0'):
 
-        self.w_ih_node = w_ih_node
+        with tf.device(device):
+            self.w_ih_node = w_ih_node
 
-        self.b_h_node = b_h_node
-        self.w_hh_node = w_hh_node
+            self.b_h_node = b_h_node
+            self.w_hh_node = w_hh_node
 
-        self.b_o_node = b_o_node
-        self.w_ho_node = w_ho_node
+            self.b_o_node = b_o_node
+            self.w_ho_node = w_ho_node
 
-        self.delta_w_node = delta_w_node
-        # self.stateful_node = stateful_node
+            self.delta_w_node = delta_w_node
+            # self.stateful_node = stateful_node
 
-        self.n_inputs = len(input_coords)
-        self.input_coords = torch.tensor(
-            input_coords, dtype=torch.float32, device=device)
+            self.n_inputs = len(input_coords)
+            self.input_coords = tf.convert_to_tensor(input_coords, preferred_dtype=tf.float32)
 
-        self.n_hidden = len(hidden_coords)
-        self.hidden_coords = torch.tensor(
-            hidden_coords, dtype=torch.float32, device=device)
+            self.n_hidden = len(hidden_coords)
+            self.hidden_coords = tf.convert_to_tensor(hidden_coords, preferred_dtype=tf.float32)
 
-        self.n_outputs = len(output_coords)
-        self.output_coords = torch.tensor(
-            output_coords, dtype=torch.float32, device=device)
+            self.n_outputs = len(output_coords)
+            self.output_coords = tf.convert_to_tensor(output_coords, preferred_dtype=tf.float32)
 
-        self.weight_threshold = weight_threshold
+            self.weight_threshold = weight_threshold
 
-        self.activation = activation
+            self.activation = activation
 
-        self.batch_size = batch_size
-        self.device = device
+            self.batch_size = batch_size
+            self.device = device
         self.reset()
 
     def get_init_weights(self, in_coords, out_coords, w_node):
-        (x_out, y_out), (x_in, y_in) = get_coord_inputs(in_coords, out_coords)
+        with tf.device(self.device):
+            (x_out, y_out), (x_in, y_in) = get_coord_inputs(in_coords, out_coords)
 
-        n_in = len(in_coords)
-        n_out = len(out_coords)
+            n_in = len(in_coords)
+            n_out = len(out_coords)
 
-        zeros = torch.zeros(
-            (n_out, n_in), dtype=torch.float32, device=self.device)
+            zeros = tf.zeros((n_out, n_in), dtype=tf.float32)
 
-        weights = w_node(x_out=x_out, y_out=y_out, x_in=x_in, y_in=y_in,
-                         pre=zeros, post=zeros, w=zeros)
-        return clamp_weights_(weights, self.weight_threshold)
+            weights = w_node(x_out=x_out, y_out=y_out, x_in=x_in, y_in=y_in,
+                             pre=zeros, post=zeros, w=zeros)
+            return clamp_weights_(weights, self.weight_threshold)
 
     def reset(self):
-        with torch.no_grad():
-            self.input_to_hidden = self.get_init_weights(
-                self.input_coords, self.hidden_coords, self.w_ih_node)
+        with tf.device(self.device):
+            self.input_to_hidden = self.get_init_weights(self.input_coords, self.hidden_coords, self.w_ih_node)
 
-            bias_coords = torch.zeros(
-                (1, 2), dtype=torch.float32, device=self.device)
-            self.bias_hidden = self.get_init_weights(
-                bias_coords, self.hidden_coords, self.b_h_node).unsqueeze(0).expand(
-                    self.batch_size, self.n_hidden, 1)
+            bias_coords = tf.zeros((1, 2), dtype=tf.float32)
+            self.bias_hidden = self.get_init_weights(bias_coords, self.hidden_coords, self.b_h_node)
+            self.bias_hidden = expand(tf.expand_dims(self.bias_hidden, 0), (self.batch_size, self.n_hidden, 1))
 
-            self.hidden_to_hidden = self.get_init_weights(
-                self.hidden_coords, self.hidden_coords, self.w_hh_node).unsqueeze(0).expand(
-                    self.batch_size, self.n_hidden, self.n_hidden)
+            self.hidden_to_hidden = self.get_init_weights(self.hidden_coords, self.hidden_coords, self.w_hh_node)
+            self.hidden_to_hidden = expand(tf.expand_dims(self.hidden_to_hidden, 0),
+                                           (self.batch_size, self.n_hidden, self.n_hidden))
 
-            bias_coords = torch.zeros(
-                (1, 2), dtype=torch.float32, device=self.device)
-            self.bias_output = self.get_init_weights(
-                bias_coords, self.output_coords, self.b_o_node)
+            bias_coords = tf.zeros((1, 2), dtype=f.float32)
+            self.bias_output = self.get_init_weights(bias_coords, self.output_coords, self.b_o_node)
+            self.hidden_to_output = self.get_init_weights(self.hidden_coords, self.output_coords, self.w_ho_node)
+            self.hidden = tf.zeros((self.batch_size, self.n_hidden, 1), dtype=tf.float32)
 
-            self.hidden_to_output = self.get_init_weights(
-                self.hidden_coords, self.output_coords, self.w_ho_node)
-
-            self.hidden = torch.zeros((self.batch_size, self.n_hidden, 1),
-                                      dtype=torch.float32)
-
-            self.batched_hidden_coords = get_coord_inputs(
-                self.hidden_coords, self.hidden_coords, batch_size=self.batch_size)
-            # self.cppn_state = torch.zeros(
-            #     (self.batch_size, self.n_hidden, self.n_hidden))
+            self.batched_hidden_coords = get_coord_inputs(self.hidden_coords, self.hidden_coords,
+                                                          batch_size=self.batch_size)
 
     def activate(self, inputs):
         '''
@@ -119,21 +106,16 @@ class AdaptiveNet:
 
         returns: (batch_size, n_outputs)
         '''
-        with torch.no_grad():
-            inputs = torch.tensor(
-                inputs, dtype=torch.float32, device=self.device).unsqueeze(2)
-
+        with tf.device(self.device):
+            inputs = tf.expand_dims(tf.convert_to_tensor(inputs, dtype=tf.float32), 2)
             self.hidden = self.activation(self.input_to_hidden.matmul(inputs) +
                                           self.hidden_to_hidden.matmul(self.hidden) +
                                           self.bias_hidden)
 
-            outputs = self.activation(
-                self.hidden_to_output.matmul(self.hidden) +
-                self.bias_output)
+            outputs = self.activation(self.hidden_to_output @ self.hidden + self.bias_output)
 
-            hidden_outputs = self.hidden.expand(
-                self.batch_size, self.n_hidden, self.n_hidden)
-            hidden_inputs = hidden_outputs.transpose(1, 2)
+            hidden_outputs = expand(self.hidden, (self.batch_size, self.n_hidden, self.n_hidden))
+            hidden_inputs = tf.transpose(hidden_outputs, perm=[0, 2, 1])
 
             (x_out, y_out), (x_in, y_in) = self.batched_hidden_coords
 
@@ -141,9 +123,8 @@ class AdaptiveNet:
                 x_out=x_out, y_out=y_out, x_in=x_in, y_in=y_in,
                 pre=hidden_inputs, post=hidden_outputs,
                 w=self.hidden_to_hidden)
-            # self.cppn_state = self.stateful_node.get_activs()
 
-        return outputs.squeeze(2)
+        return tf.squeeze(outputs, 2)
 
     @staticmethod
     def create(genome,
