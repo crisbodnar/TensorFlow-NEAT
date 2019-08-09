45274c29073fb5b6af814f5dc73156c130b6a3bc
Nick Williams <17623814+nickwilliamsnewby@users.noreply.github.com>
2019-08-09 15:48:24 -0500

    substrate added
    added substrate to support hyperneat and es hyperneat algorithms

---
+++ tf_neat/substrate.py
@@ -0,0 +1,7 @@
+class Substrate(object):
+
+    def __init__(self, input_coordinates, output_coordinates, hidden_coordinates=(), res=10.0):
+        self.input_coordinates = input_coordinates
+        self.hidden_coordinates = hidden_coordinates        
+        self.output_coordinates = output_coordinates
+        self.res = res
